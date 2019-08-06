b2a4e8a719b3f9f7465293116c988f6748804b75
Navin <navin007.a@gmail.com>
2019-08-06 11:33:24 +0000

    Deploy navin-navi/redux-github-commits-history-chart to github.com/navin-navi/redux-github-commits-history-chart.git:gh-pages

---
+++ precache-manifest.aaaa9306d72f50596ba55a5565a10905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31866042aaa86daf75256cc3210ec914",
+    "url": "https://navin-navi.github.io/redux-github-commits-history-chart/index.html"
+  },
+  {
+    "revision": "5adfa9980e875258b7b5",
+    "url": "https://navin-navi.github.io/redux-github-commits-history-chart/static/css/main.76161976.chunk.css"
+  },
+  {
+    "revision": "4ee31a82fa0e02316bf1",
+    "url": "https://navin-navi.github.io/redux-github-commits-history-chart/static/js/2.7d5d1692.chunk.js"
+  },
+  {
+    "revision": "5adfa9980e875258b7b5",
+    "url": "https://navin-navi.github.io/redux-github-commits-history-chart/static/js/main.e3df465a.chunk.js"
+  },
+  {
+    "revision": "aa0f40c842bc69cd1478",
+    "url": "https://navin-navi.github.io/redux-github-commits-history-chart/static/js/runtime~main.4f0697a5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://navin-navi.github.io/redux-github-commits-history-chart/precache-manifest.aaaa9306d72f50596ba55a5565a10905.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://navin-navi.github.io/redux-github-commits-history-chart/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
